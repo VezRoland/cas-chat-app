14c855f12c1af54a3d328d372ff360c6dbd5ea11
Vezsenyi Roland <82434540+VezRoland@users.noreply.github.com>
2026-03-12 13:51:53 +0100

    create base sign in page and fix some issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
-import { SignUpPage } from './routes/sign-up-page';
+import { SignUpPage } from './routes/sign-up/sign-up-page';
+import { SignInPage } from './routes/sign-in/sign-in-page';
 
 export const routes: Routes = [
   {
     path: 'signup',
     component: SignUpPage,
     title: 'Sign Up',
+  },
+  {
+    path: 'signin',
+    component: SignInPage,
+    title: 'Sign In',
   },
 ];
