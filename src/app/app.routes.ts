0bdf943d96b6d5998443c0ec8557b9b52240fdce
Vezsenyi Roland <82434540+VezRoland@users.noreply.github.com>
2026-03-12 14:26:36 +0100

    create a barebones chat rooms page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { SignUpPage } from './routes/sign-up/sign-up-page';
 import { SignInPage } from './routes/sign-in/sign-in-page';
+import { RoomsPage } from './routes/rooms/rooms-page';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path: 'signin',
     component: SignInPage,
     title: 'Sign In',
+  },
+  {
+    path: 'rooms',
+    component: RoomsPage,
+    title: 'Chat Rooms',
   },
 ];
